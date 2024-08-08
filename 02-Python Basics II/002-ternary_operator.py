db4e989410e566a4c27a43abda6a7c88f990ad25
VY <veasna.youphon@live.com>
2024-08-08 14:09:22 -0700

    complete ternary operator

---
+++ 02-Python Basics II/002-ternary_operator.py
@@ -0,0 +1,9 @@
+#Ternary Operator; it's use similarly to conditional expression
+
+#condition_if_true if condition else condition_if_else
+# variable_to_store = runthis if condition_evaluate_to_true else runthis
+
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+
+print(can_message)
