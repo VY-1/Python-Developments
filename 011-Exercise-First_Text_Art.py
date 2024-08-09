ce394e5d35cbc43fe4888d81a62cd7b331b3a216
VY <veasna.youphon@live.com>
2024-08-09 00:22:44 -0700

    complete exercise-first-text-art

---
+++ 011-Exercise-First_Text_Art.py
@@ -0,0 +1,19 @@
+#Exercise
+
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+
+]
+
+for row in picture:
+    for col in row:
+        if(col == 1):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
