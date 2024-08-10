680aefdf639e38e3ea67221e27bef3976b594441
VY <veasna.youphon@live.com>
2024-08-09 22:50:55 -0700

    complete methods vs functions

---
+++ 02-Python Basics II/016-methods_vs_functions.py
@@ -0,0 +1,17 @@
+#Methods vs Functions
+
+# list()
+# print()
+# max()
+# min()
+# input()
+
+def some_random_stuff():
+    pass
+
+some_random_stuff()
+
+
+print("======Method======")
+print('hellllooo'.capitalize())
+
