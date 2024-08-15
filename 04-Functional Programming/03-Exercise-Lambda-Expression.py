d56465f59c34bb1d4fc7a7f03ab595e478763420
VY <veasna.youphon@live.com>
2024-08-14 23:00:03 -0700

    complete lambda exercise

---
+++ 04-Functional Programming/03-Exercise-Lambda-Expression.py
@@ -0,0 +1,12 @@
+# Exercise with Lambda Expression
+
+# Square value in my_list
+my_list = [5, 4, 3]
+
+print(list(map(lambda item: item**2, my_list)))
+
+#List sorting tuple by second value
+
+a=[(0,2), (4,3), (9,9), (10,-1)]
+a.sort(key=lambda x: x[1])
+print(a)
